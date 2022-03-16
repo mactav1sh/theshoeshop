1f20562ec9326f1fd78713753424fc924e99f9f6
mactav1sh <mactavish104@gmail.com>
2022-03-16 18:04:59 +0200

    Added slider section with some CSS

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 // DOM ELEMENTS
 const switchBtn = document.querySelector(".switch--input");
 const headerImg = document.querySelector(".header-img");
+const btnLeft = document.querySelector(".btn-left");
+const btnRight = document.querySelector(".btn-right");
+const slides = document.querySelectorAll(".slide");
 
 const darkMode = function (e) {
   if (e.target.checked) {
@@ -15,3 +18,42 @@ const darkMode = function (e) {
   }
 };
 switchBtn.addEventListener("change", darkMode);
+
+// CAROUSEL
+
+let currentSlide = 1;
+//adding transform to slides
+const displaySlides = function () {
+  slides.forEach((slide, i) => {
+    slide.setAttribute("style", `transform: translateX(${101 * i}%)`);
+  });
+};
+// 0 , 100% , 200%
+displaySlides();
+
+function nextSlide() {
+  // Check current slide if it's the last
+  if (currentSlide === slides.length) currentSlide = 0; // this will restart the slider to the first slide by making the transforms 0,200,100
+  // cur slide = 1 -> 0,100,200 -> -100, 0, 100
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(${101 * i - currentSlide * 101}%)`;
+  });
+  currentSlide++;
+}
+// after first click 0 -> A(-100) , 100 -> B(0) , 200 -> C(100)
+
+function previousSlide() {
+  // cur slide = 2
+  if (currentSlide === 1) currentSlide = slides.length + 1; // this will restart the slider to the end slide by making the transforms -200,-100,0
+  currentSlide--; // cur slide = 1
+
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(${
+      101 * (i + 1) - currentSlide * 101
+    }%)`; // 0
+  });
+}
+// after first (after the first Slide click) click -100 -> A(0) , 0 -> B(100) , 100 -> C(200)
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", previousSlide);
